c662569e6be5c23dbcb7a7287ab4446a0b3946ff
shuftipro <support@shuftipro.com>
2023-01-30 18:15:32 +0500

    auto capture suppport added

--- Package.swift
+++ Package.swift
@@ -1,31 +1,48 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ShuftiPro",
-    platforms: [.iOS(.v11) ],
+    platforms: [.iOS(.v13) ],
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShuftiPro",
-            targets: ["ShuftiPro"]),
+            targets: ["PackageDependencies" , "ShuftiPro"]),
+        
     ],
     dependencies: [
+    
+        .package(url: "https://github.com/airbnb/lottie-ios", from: "4.1.2" ),
+        
+        .package(url: "https://github.com/socketio/socket.io-client-swift", from: "16.0.1" ),
+
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
+   
+
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    
         
         .binaryTarget(
                 name: "ShuftiPro",
                 url:"https://github.com/shuftipro/shuftipro-framework/raw/main/ShuftiPro.xcframework.zip",
-                checksum: "95fb9f5434d354a630dc6a324639bb73d2f5658f2d176fa7cb6186ad71dd03b7"
-                      )
+                checksum: "5f6a39cef870c5138bef17040557ad6f7d7f6e0982505d29eaa566e8a12fcc68"
+                      ),
+    
+        
+        .target(
+            name: "PackageDependencies",
+            dependencies: [ .product(name: "SocketIO", package: "socket.io-client-swift") ,  .product(name: "Lottie", package: "lottie-ios") ],
+            path: "Sources",
+            resources: [
+                .process("Resource/Media.xcassets")
+            ]),
+   
     ]
 )
-
---
+++ Sources/ShuftiPro/ShuftiPro.swift
@@ -0,0 +1,8 @@
+import Starscream
+import Lottie
+//import ShuftiPro
+//import Lottie
+
+struct ShuftiPro {
+    var text = "Hello, World!"
+}
